d11d2646d613a2b5384931d1bd36ba997a4e1a94
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-03 16:17:10 +0100

    dangerouslySetInnerHTML

--- src/components/NoteDisplay/index.jsx
+++ src/components/NoteDisplay/index.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import Showdown from 'showdown';
+const converter = new Showdown.Converter();
 
 const NoteDisplay = (props) => {
 
-  const capitalizze = () => {
-    return props.textBrut.toUpperCase();
+  const createMarkup = () => {
+    let conversion = converter.makeHtml(props.textBrut);
+    return { __html: conversion };
   };
     
   return (
     <section id="NoteDisplay">
       <h1>NoteDisplay</h1>
-      <p>{capitalizze()}</p>
+      <div dangerouslySetInnerHTML={createMarkup()} />
     </section>
   );
 };
