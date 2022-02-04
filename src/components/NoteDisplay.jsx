621ebf277ec95fccd43e1c5cdb7067378df70ba8
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-04 05:27:45 +0100

    adds initial note

--- src/components/NoteDisplay.jsx
+++ src/components/NoteDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Showdown from 'showdown';
 const converter = new Showdown.Converter();
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+localStorage.setItem('Hello Friend', 'Welcome to the notePad like app !');
+
 ReactDOM.render(<App /> , document.getElementById('root'));
