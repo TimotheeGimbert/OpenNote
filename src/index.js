253944a58d126510e6f8615fa839834b9c17e86e
TimotheeGimbert <timothee.gimbert@free.fr>
2022-04-07 15:45:40 +0200

    edit to force rebuild vercel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const initialTitle = 'Bonjour !';
-const initialContent = 'Vous souhaitez prendre des notes ? \n ***Tester notre superbe Markdown*** \n \n Cest facile, pratique, et gratuit !';
+const initialContent = 'Vous souhaitez prendre des notes? \n ***Tester notre superbe Markdown*** \n \n Cest facile, pratique, et gratuit !';
 localStorage.setItem(initialTitle, initialContent);
 
 ReactDOM.render(<App /> , document.getElementById('root'));
